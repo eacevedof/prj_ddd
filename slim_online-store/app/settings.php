740580a3929209c72320401d04f1791d7875078b
Eduardo A.F <eacevedof@yahoo.es>
2019-09-22 19:44:27 +0200

    doctrine 2 no tira, no hay doc para slim 4

--- slim_online-store/app/settings.php
+++ slim_online-store/app/settings.php
@@ -1,4 +1,5 @@
 <?php
+//app/settings.php
 declare(strict_types=1);
 
 use DI\ContainerBuilder;
---
+++ slim_online-store/cli-config.php
@@ -0,0 +1,14 @@
+<?php
+//doctrine
+//cli-config.php
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Slim\Container;
+
+/** @var Container $container */
+$container = require_once __DIR__ . '/bootstrap.php';
+
+ConsoleRunner::run(
+    ConsoleRunner::createHelperSet($container[EntityManager::class])
+);
