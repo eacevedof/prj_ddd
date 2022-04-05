39985068405ad2040551b02c9437b352e6e95470
ioedumac <eacevedof@yahoo.es>
2022-04-05 10:42:40 +0200

    git ignore

--- blog-publish-mvc/src/autoload.php
+++ blog-publish-mvc/src/autoload.php
@@ -23,7 +23,6 @@ function pr($var="",$sTitle=NULL)
 $pathappds = dirname(__FILE__);
 set_include_path(get_include_path().":".$pathappds);
 spl_autoload_register(function(string $nsclass) use ($pathappds) {
-    //nsclass: App\\Blog\\Infrastructure\\PostController
     $nsclass = str_replace(["App\\","\\"],["","/"], $nsclass);
     $nsclass .= ".php";
     include_once $nsclass;
