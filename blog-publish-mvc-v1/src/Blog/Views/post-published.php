f6cf621ccba1a38443fa34f969393491770e0149
ioedumac <eacevedof@yahoo.es>
2022-04-05 12:53:25 +0200

    mvc-1

--- blog-publish-mvc-v1/src/Blog/Views/post-published.php
+++ blog-publish-mvc-v1/src/Blog/Views/post-published.php
@@ -2,6 +2,7 @@
 /**
  * @var App\Blog\Models\PostEntity $post;
  */
+if (isset($error)) dd($error, "Some error occurred:");
 ?>
 <h1>Post status: <?=$post->status() ? "Published" : "Not published"?></h1>
 <h2><?=$post->title()?></h2>
