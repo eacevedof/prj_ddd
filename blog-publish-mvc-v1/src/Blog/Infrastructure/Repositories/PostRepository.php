b7b7f72414667f8d4036e56825172a10b20a24d3
ioedumac <eacevedof@yahoo.es>
2022-04-06 19:23:15 +0200

    mvc-3 move to ddd split in services

--- blog-publish-mvc-v1/src/Blog/Infrastructure/Repositories/PostRepository.php
+++ blog-publish-mvc-v1/src/Blog/Infrastructure/Repositories/PostRepository.php
@@ -10,7 +10,7 @@ final class PostRepository
         return new PostEntity(
             $id,
             "Qué es Lorem Ipsum?",
-            "  Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. <br/>
+            "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. <br/>
   Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T.
   persona que se dedica a la imprenta) <br/>
   desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
